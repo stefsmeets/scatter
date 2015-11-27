f21530137188557b98431cf7decb6e02d500e49b
Stef Smeets <stef.smeets@mat.ethz.ch>
2015-11-28 00:10:10 +0100

    Code cleanup (PEP-8)

--- setup.py
+++ setup.py
@@ -6,32 +6,31 @@ from os import path
 # www.pythonhosted.org/setuptools/setuptools.html
 
 setup(
-	name = "scatter",
-	version = "1.0.0",
-	description = "A python tool to plot and output atomic scattering factors",
+    name="scatter",
+    version="1.0.0",
+    description="A python tool to plot and output atomic scattering factors",
 
-	author = "Stef Smeets",
-	author_email = "stef.smeets@mat.ethz.ch",
-	license = "GPL",
-	url = "https://github.com/stefsmeets/scatter",
+    author="Stef Smeets",
+    author_email="stef.smeets@mat.ethz.ch",
+    license="GPL",
+    url="https://github.com/stefsmeets/scatter",
 
-	classifiers=[
-		'Programming Language :: Python :: 2.7',
-		],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
 
-	packages = ["scatter"],
+    packages=["scatter"],
 
-	install_requires = ["numpy", "matplotlib"],
+    install_requires=["numpy", "matplotlib"],
 
-	package_data = {
-	"": ["LICENCE",  "readme.md", "setup.py", "scatter.py"],
-		},
+    package_data={
+        "": ["LICENCE",  "readme.md", "setup.py", "scatter.py"],
+    },
 
-	entry_points = {
-		'console_scripts': [
-			'scatter = scatter.scatter:main',
-			]
-		}
+    entry_points={
+        'console_scripts': [
+            'scatter = scatter.scatter:main',
+        ]
+    }
 
-	)
-
+)
